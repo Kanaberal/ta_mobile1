fd3655d4ae01427ca17896469e3f90876ee250bf
Kanaberal <aden286chara@gmail.com>
2024-10-06 13:53:08 +0700

    update

---
+++ ta_mobile/app/src/main/java/com/example/tamobile/halaman_awal.kt
@@ -0,0 +1,11 @@
+package com.example.tamobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class halaman_awal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_halaman_awal)
+    }
+}
