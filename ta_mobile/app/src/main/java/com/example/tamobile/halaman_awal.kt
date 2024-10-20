131c913e7c5d041c5840f1ef3ad86d5aaa008c74
Kanaberal <aden286chara@gmail.com>
2024-10-20 14:15:39 +0700

    login awal,login page layout done

--- ta_mobile/app/src/main/java/com/example/tamobile/halaman_awal.kt
+++ ta_mobile/app/src/main/java/com/example/tamobile/halaman_awal.kt
@@ -1,11 +1,26 @@
 package com.example.tamobile
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
 
+import android.content.Intent
+
+import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
+
 class halaman_awal : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_halaman_awal)
+
+
+        val backBtn = findViewById<ImageButton>(R.id.nextbtn)
+        backBtn.setOnClickListener {
+
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- ta_mobile/app/src/main/java/com/example/tamobile/login.kt
+++ ta_mobile/app/src/main/java/com/example/tamobile/login.kt
@@ -1,11 +1,45 @@
 package com.example.tamobile
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 class login : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
+        val backBtn = findViewById<ImageButton>(R.id.backbutton)
+        val loginBtn = findViewById<ImageButton>(R.id.loginbtn)
+        val inputNama = findViewById<EditText>(R.id.inputnama)
+        val inputPassword = findViewById<EditText>(R.id.inputpw)
+
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this, halaman_awal::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
+        loginBtn.setOnClickListener {
+            val username = inputNama.text.toString()
+            val password = inputPassword.text.toString()
+
+
+            if (username == "admin" && password == "admin") {
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                
+                Toast.makeText(this, "Username atau Password salah", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
